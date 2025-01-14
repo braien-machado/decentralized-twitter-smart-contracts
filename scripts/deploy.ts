649c965de358cce54e944a1e502544bca99fe470
Braien Machado de Paula <braienmp@outlook.com>
2025-01-14 20:36:46 -0300

    chore: create deploy script to interact with persistent blockchain

---
+++ scripts/deploy.ts
@@ -0,0 +1,26 @@
+import hre from 'hardhat';
+
+const main = async () => {
+  const [owner] = await hre.ethers.getSigners();
+  const accountBalance = await owner.provider.getBalance(owner.address);
+
+  console.log('Deployed by:', owner.address);
+  console.log('Account balance:', accountBalance.toString());
+
+  const post = await hre.ethers.deployContract('Post');
+  await post.waitForDeployment();
+
+  console.log('Post deployed to:', post.target);
+}
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+}
+
+runMain()
