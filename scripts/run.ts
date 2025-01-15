b29fbe78b6b913480bde93ae41f0a8d4b8a02b59
Braien Machado de Paula <braienmp@outlook.com>
2025-01-15 15:36:29 -0300

    chore: update run.ts to test prize winning and balance updates

--- scripts/run.ts
+++ scripts/run.ts
@@ -2,16 +2,23 @@ import hre from "hardhat";
 
 const main = async () => {
   const [owner] = await hre.ethers.getSigners();
-  const post = (await hre.ethers.deployContract('Post'));
+  const post = (await hre.ethers.deployContract('Post', {
+    value: hre.ethers.parseEther('0.1')
+  }));
   await post.waitForDeployment();
 
   console.log('Post deployed to:', post.target);
   console.log('Deployed by:', owner.address);
 
-  await post.getTotalPosts();
+  let contractBalance = await hre.ethers.provider.getBalance(post.target);
+  
+  console.log('Contract balance:', hre.ethers.formatEther(contractBalance))
 
-  const waveTxn = await post.createPost();
+  const waveTxn = await post.createPost('Sending 1# post');
   await waveTxn.wait();
+
+  contractBalance = await hre.ethers.provider.getBalance(post.target);
+  console.log('Contract balance after sending post:', hre.ethers.formatEther(contractBalance))
 
   await post.getTotalPosts();
 }
