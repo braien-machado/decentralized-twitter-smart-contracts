4733341b02c0ce234e880cebd227ed4b953fd60d
Braien Machado de Paula <braienmp@outlook.com>
2025-01-15 13:52:15 -0300

    chore: use env variables to set staging network on hardhat config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,17 @@
+import dotenv from 'dotenv'
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
+dotenv.config()
+
 const config: HardhatUserConfig = {
   solidity: "0.8.28",
+  networks: {
+    staging: {
+      url: process.env.APP_URL,
+      accounts: [process.env.PRIVATE_KEY!]
+    }
+  }
 };
 
 export default config;
